e1ed07dcd1000a928fd19574fd4ec6e810b0578c
ljt <liujiatai2016@163.com>
2019-06-25 17:52:11 +0800

    分组

--- src/main/java/com/java8/study/chap2/FilteringApples.java
+++ src/main/java/com/java8/study/chap2/FilteringApples.java
@@ -7,16 +7,27 @@ import lombok.ToString;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class FilteringApples{
 
     public static void main(String[] args) {
         List<Apple> inventory = Arrays.asList(new Apple(80,"green"), new Apple(155, "green"), new Apple(120, "red"));
-        List<Apple> greenApples = filterApples(inventory, (apple) -> "red".equals(apple.getColor()));
-        System.out.println(greenApples);
+        List<Apple> redApples = filterApples(inventory, (apple) -> "red".equals(apple.getColor()));
+        System.out.println("redApples: " + redApples);
 
         List<Apple> heavyApples = filterApples(inventory, Apple::isHeavyApple);
-        System.out.println(heavyApples);
+        System.out.println("heavyApples: " + heavyApples);
+
+        List<Apple> greenApples = inventory.stream().filter(a -> "green".equals(a.getColor())).collect(Collectors.toList());
+        System.out.println("greenApples: " + greenApples);
+        System.out.println("-------------------------------------------------");
+        // 分组
+        Map<String, List<Apple>> map = inventory.stream().collect(Collectors.groupingBy(Apple::getColor));
+        for (String key : map.keySet()) {
+            System.out.println(map.get(key));
+        }
     }
 
     public static List<Apple> filterApplesByColor(List<Apple> inventory, String color) {
