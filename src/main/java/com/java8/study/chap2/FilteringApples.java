8f128776a27f2bd08e5d769e7607b04463a943f9
ljt <liujiatai2016@163.com>
2019-06-28 16:07:18 +0800

    方法泛型,通用

--- src/main/java/com/java8/study/chap2/FilteringApples.java
+++ src/main/java/com/java8/study/chap2/FilteringApples.java
@@ -14,18 +14,42 @@ public class FilteringApples{
 
     public static void main(String[] args) {
         List<Apple> inventory = Arrays.asList(new Apple(80,"green"), new Apple(155, "green"), new Apple(120, "red"));
-        List<Apple> redApples = filterApples(inventory, (apple) -> "red".equals(apple.getColor()));
+
+        /**
+         * 传递类(策略)
+         */
+        List<Apple> greenApples = filterApples(inventory, new AppleGreenColorPredicate());
+        System.out.println("greenApples: " + greenApples);
+
+        /**
+         * 匿名类
+         */
+        List<Apple> blueApples = filterApples(inventory, new ApplePredicate<Apple>() {
+            @Override
+            public boolean test(Apple apple) {
+                return "blue".equalsIgnoreCase(apple.getColor());
+            }
+        });
+        System.out.println(blueApples);
+
+        /**
+         * lambda
+         */
+        List<Apple> redApples = filterApples(inventory, (Apple apple) -> "red".equals(apple.getColor()));
         System.out.println("redApples: " + redApples);
 
         List<Apple> heavyApples = filterApples(inventory, Apple::isHeavyApple);
         System.out.println("heavyApples: " + heavyApples);
 
-        List<Apple> greenApples = inventory.stream().filter(a -> "green".equals(a.getColor())).collect(Collectors.toList());
-        System.out.println("greenApples: " + greenApples);
+        List<Apple> greenApples2 = inventory.stream().filter(a -> "green".equals(a.getColor())).collect(Collectors.toList());
+        System.out.println("greenApples2: " + greenApples2);
         System.out.println("-------------------------------------------------");
         // 分组
         Map<String, List<Apple>> map = inventory.stream().collect(Collectors.groupingBy(Apple::getColor));
 
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> nums = filter(numbers, i -> i > 3);
+        System.out.println(nums);
         System.out.println(map);
     }
 
@@ -60,6 +84,18 @@ public class FilteringApples{
         return apples;
     }
 
+    public static List<Apple> filterApples(List<Apple> inventory, String color,
+                                           int weight, boolean flag) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple: inventory){
+            if ( (flag && apple.getColor().equals(color)) ||
+                    (!flag && apple.getWeight() > weight) ){
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
     /**
      * 根据重量挑选苹果
      * @param inventory
@@ -92,22 +128,63 @@ public class FilteringApples{
         return apples;
     }
 
-    @Data
-    @AllArgsConstructor
-    @ToString
-    public static class Apple {
-        private int weight = 0;
-        private String color;
-
-        public static boolean isGreenApple(Apple apple) {
-            return "green".equals(apple.getColor());
+    /**
+     * 方法抽象泛型
+     * @param list
+     * @param p
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> filter(List<T> list, ApplePredicate<T> p) {
+        List<T> result = new ArrayList<>();
+        for (T t : list) {
+            if (p.test(t)) {
+                result.add(t);
+            }
         }
-        public static boolean isHeavyApple(Apple apple) {
-            return apple.getWeight() > 150;
-        }
+        return result;
     }
+}
 
-    interface ApplePredicate<T> {
-        boolean test(T t);
+
+@Data
+@AllArgsConstructor
+@ToString
+class Apple {
+    private int weight = 0;
+    private String color;
+
+    public static boolean isGreenApple(Apple apple) {
+        return "green".equals(apple.getColor());
+    }
+    public static boolean isHeavyApple(Apple apple) {
+        return apple.getWeight() > 150;
+    }
+}
+/**
+ * 选择标准(谓语)
+ * @param <T>
+ */
+interface ApplePredicate<T> {
+    boolean test(T t);
+}
+
+/**
+ * 重量
+ */
+class AppleHeavyWeightPredicate implements ApplePredicate<Apple> {
+
+    @Override
+    public boolean test(Apple apple) {
+        return apple.getWeight() > 150;
+    }
+}
+
+/**
+ * 绿色
+ */
+class AppleGreenColorPredicate implements ApplePredicate<Apple>{
+    public boolean test(Apple apple){
+        return "green".equals(apple.getColor());
     }
 }
